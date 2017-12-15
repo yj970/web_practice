8e5c5199f1fb0f6c366bf0b333c87d8481e9968b
杨杰 <yangjie@kkkk.local>
2017-12-15 18:45:02 +0800

    第一次提交

---
+++ web仿写/百度一下/js/baiduyixia.js
@@ -0,0 +1,12 @@
+/*输入框获取焦点*/
+function search_focus() {
+	var content = document.getElementsByClassName('content')[0];
+	content.style.borderColor = '#3388FF';
+}
+/*输入框失去焦点*/
+function search_blur() {
+	var content = document.getElementsByClassName('content')[0];
+	content.style.borderColor = '#DBDBDB';
+}
+
+
---
+++ web仿写/网易新闻/js/163news.js
@@ -0,0 +1,176 @@
+$(function() {
+	$('#top_bar_block_1_1').rotate({
+		bind: {
+			mouseover: function() {
+				$('#ver_arr_1').rotate({
+					animateTo: 180
+				});
+				$('#dialog_wangyi').css({
+					'display': 'block'
+				});
+			},
+			mouseout: function() {
+				$('#ver_arr_1').rotate({
+					animateTo: 0
+				});
+				$('#dialog_wangyi').css({
+					'display': 'none'
+				});
+			}
+		}
+	});
+
+	$('#top_bar_block_1_2').rotate({
+		bind: {
+			mouseover: function() {
+				$('#ver_arr_2').rotate({
+					animateTo: 180
+				});
+				$('#dialog_vedio').css({
+					'display': 'block'
+				});
+			},
+			mouseout: function() {
+				$('#ver_arr_2').rotate({
+					animateTo: 0
+				});
+				$('#dialog_vedio').css({
+					'display': 'none'
+				});
+			}
+		}
+	});
+
+	$('#top_bar_block_2_1').rotate({
+		bind: {
+			mouseover: function() {
+				$('#ver_arr_3').rotate({
+					animateTo: 180
+				});
+			},
+			mouseout: function() {
+				$('#ver_arr_3').rotate({
+					animateTo: 0
+				});
+			}
+		}
+	});
+
+	$('#top_bar_block_2_2').rotate({
+		bind: {
+			mouseover: function() {
+				$('#ver_arr_4').rotate({
+					animateTo: 180
+				});
+			},
+			mouseout: function() {
+				$('#ver_arr_4').rotate({
+					animateTo: 0
+				});
+			}
+		}
+	});
+
+	$('#top_bar_block_2_3').rotate({
+		bind: {
+			mouseover: function() {
+				$('#ver_arr_5').rotate({
+					animateTo: 180
+				});
+			},
+			mouseout: function() {
+				$('#ver_arr_5').rotate({
+					animateTo: 0
+				});
+			}
+		}
+	});
+
+	$('#top_bar_block_1_3').rotate({
+		bind: {
+			mouseover: function() {
+				$('#ver_arr_6').rotate({
+					animateTo: 180
+				});
+			},
+			mouseout: function() {
+				$('#ver_arr_6').rotate({
+					animateTo: 0
+				});
+			}
+		}
+	});
+
+	$('#top_bar_block_1_4').rotate({
+		bind: {
+			mouseover: function() {
+				$('#ver_arr_7').rotate({
+					animateTo: 180
+				});
+			},
+			mouseout: function() {
+				$('#ver_arr_7').rotate({
+					animateTo: 0
+				});
+			}
+		}
+	});
+
+	$('#top_bar_block_1_5').rotate({
+		bind: {
+			mouseover: function() {
+				$('#ver_arr_8').rotate({
+					animateTo: 180
+				});
+			},
+			mouseout: function() {
+				$('#ver_arr_8').rotate({
+					animateTo: 0
+				});
+			}
+		}
+	});
+
+	$('#top_bar_block_3_1').rotate({
+		bind: {
+			mouseover: function() {
+				$('#ver_arr_9').rotate({
+					animateTo: 180
+				});
+			},
+			mouseout: function() {
+				$('#ver_arr_9').rotate({
+					animateTo: 0
+				});
+			}
+		}
+	});
+});
+
+$(document).ready(function() {
+	$('#dialog_wangyi').mouseover(function() {
+		$('#dialog_wangyi').css({'display': 'block'});
+	});
+	$('#dialog_wangyi').mouseout(function() {
+		$('#dialog_wangyi').css({'display': 'none'});
+	});
+
+	$('#dialog_vedio').mouseover(function() {
+		$('#dialog_vedio').css({'display': 'block'});
+	});
+	$('#dialog_vedio').mouseout(function() {
+		$('#dialog_vedio').css({'display': 'none'});
+	});
+	$('#login_in').mouseover(function() {
+		$('#dialog_login').css({'display': 'flex'});
+	});
+	$('#login_in').mouseout(function() {
+		$('#dialog_login').css({'display': 'none'});
+	});
+	$('#dialog_login').mouseover(function() {
+		$('#dialog_login').css({'display': 'flex'});
+	});
+	$('#dialog_login').mouseout(function() {
+		$('#dialog_login').css({'display': 'none'});
+	});
+});
